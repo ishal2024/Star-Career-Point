7be1ea85cafe27d81a500c7335675e363a645a31
ishal2024 <jhav8422@gmail.com>
2026-04-03 10:29:22 +0530

    cors error fixed

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended : true}))
 
 app.use(
   cors({
-    origin: ["http://localhost:5173" , "https://star-career-point.vercel.app/"],
+    origin: ["http://localhost:5173" , "https://star-career-point.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
