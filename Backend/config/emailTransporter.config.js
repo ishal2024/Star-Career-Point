a402f6bbc1168ecf9da238dbd74e7657c1f536b3
ishal2024 <jhav8422@gmail.com>
2026-04-04 14:06:26 +0530

    dns update

--- Backend/config/emailTransporter.config.js
+++ Backend/config/emailTransporter.config.js
@@ -7,8 +7,8 @@ dns.setDefaultResultOrder("ipv4first");
 export function getTransporter() {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com", // ✅ use host instead of service
-    port: 465,              // ✅ secure port
-    secure: true,           // ✅ required for 465
+    port: 587,              // ✅ secure port
+    secure: false,           // ✅ required for 465
     auth: {
       user: process.env.YOUR_MAIL,
       pass: process.env.EMAIL_PASSWORD, // ⚠️ no spaces in app password
