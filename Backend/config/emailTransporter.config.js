5de6472b7a7c4733b449e2fe723204b138db5e91
ishal2024 <jhav8422@gmail.com>
2026-04-04 14:44:48 +0530

    email tarnspotter config

--- Backend/config/emailTransporter.config.js
+++ Backend/config/emailTransporter.config.js
@@ -1,14 +1,11 @@
 import nodemailer from "nodemailer";
-import dns from "dns";
 
-// 🔥 Force IPv4 (fix for Render ENETUNREACH error)
-dns.setDefaultResultOrder("ipv4first");
 
 export function getTransporter() {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com", // ✅ use host instead of service
-    port: 587,              // ✅ secure port
-    secure: false,           // ✅ required for 465
+    port: 465,              // ✅ secure port
+    secure: true,           // ✅ required for 465
     auth: {
       user: process.env.YOUR_MAIL,
       pass: process.env.EMAIL_PASSWORD, // ⚠️ no spaces in app password
