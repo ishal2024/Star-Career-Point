bc535e146850e67a63bb0cca09a5e10b857dc8d3
ishal2024 <jhav8422@gmail.com>
2026-08-03 19:57:49 +0530

    send mail to updated

--- Backend/controllers/Gurgaon Real Estate/sendMail.controller.js
+++ Backend/controllers/Gurgaon Real Estate/sendMail.controller.js
@@ -17,7 +17,7 @@ export async function sendMail(req,res){
 
         const mail = {
             from: process.env.GURGAON_REAL_ESTATE_RESEND_MAIL,
-            to: "jhav8422@gmail.com",
+            to: "mayomi4942@davopa.com",
             subject: "New Enquiry",
 
             text: `
