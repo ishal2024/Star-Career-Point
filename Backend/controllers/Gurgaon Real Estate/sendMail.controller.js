9b139855191675c8edf804178d928d4a587ff1ca
ishal2024 <jhav8422@gmail.com>
2026-08-03 19:44:32 +0530

    Gurgaon real estate send mail added

---
+++ Backend/controllers/Gurgaon Real Estate/sendMail.controller.js
@@ -0,0 +1,63 @@
+import { Resend } from "resend";
+
+
+
+export async function sendMail(req,res){
+    try {
+        const {name , userMail , number , subject , message} = req?.body
+
+        if (!name || !userMail || !subject || !message) {
+            return res.status(400).json({
+                status: false,
+                message: "Name, Email and Message are required",
+            });
+        }
+
+        const resend = new Resend(process.env.GURGAON_REAL_ESTATE_API_KEY);
+
+        const mail = {
+            from: process.env.GURGAON_REAL_ESTATE_RESEND_MAIL,
+            to: "jhav8422@gmail.com",
+            subject: "New Enquiry",
+
+            text: `
+                    New Enquiry Received:
+
+                    Name: ${name}
+                    Email: ${userMail}
+                    Contact: ${number || "Not provided"}
+
+                    subject: New Enquiry - ${subject}
+
+                    Message:
+                    ${message}
+                  `,
+
+            replyTo: userMail,
+        };
+
+        const info = await resend.emails.send(mail);
+
+        if (info.error) {
+            return res.status(400).json({
+                status: false,
+                message: info.error.message,
+            });
+        }
+
+        return res.status(200).json({
+            status: true,
+            message: "Email sent Successfully",
+            data: info,
+        });
+
+
+
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            message: "Internal Server Error",
+            data: error.message,
+        });
+    }
+}
--- Backend/routes/enquire.route.js
+++ Backend/routes/enquire.route.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import enquireNowByMail from '../controllers/enquireNow.controller.js'
+import { sendMail } from '../controllers/Gurgaon Real Estate/sendMail.controller.js'
 
 const router = express.Router()
 
 router.post('' , enquireNowByMail)
 
+router.post('/realestate' , sendMail)
 
 export default router
